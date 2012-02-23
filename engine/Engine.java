35ff8a6ccbbbbc1ab038199ac091ea259bd0cd75
ivaat <taaviilp@ut.ee>
2012-02-23 19:17:43 +0200

    engine.Engine - main class!
    Signed-off-by: ivaat <taaviilp@ut.ee>

---
+++ engine/Engine.java
@@ -0,0 +1,14 @@
+
+package engine;
+
+public abstract class Engine {
+        
+    //private static UserInterface UserInterface; 
+    //private static World World; 
+    
+    public static void main(String[] args) {
+        //TODO
+    } 
+    
+
+}
